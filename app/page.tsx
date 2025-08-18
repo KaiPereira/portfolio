fac4c047607eb6975ab036ae94d2521a9bf11825
Kai Pereira <kaipereira2020@gmail.com>
2025-08-17 23:32:02 -0700

    fixed the animation and maybe trying tungsten font

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ export default function Home() {
   return (
     <div className="flex justify-between flex-col h-[100px]">
       <div className="w-full h-1/5 bg-black"></div>
-      <div className="h-3/5 text-6xl text-nowrap infinite-scroll">
-        WELCOME! WELCOME! WELCOME! WELCOME! WELCOME! WELCOME! WELCOME! WELCOME! WELCOME!
+      <div className="inline-flex flex-nowrap h-3/5 text-6xl text-nowrap">
+        <p className="infinite-scroll">WELCOME! WELCOME! WELCOME! WELCOME! WELCOME! WELCOME!</p>
+        <p className="infinite-scroll">&nbsp;WELCOME! WELCOME! WELCOME! WELCOME! WELCOME! WELCOME!</p>
       </div>
       <div className="w-full h-1/5 bg-black"></div>
     </div>
