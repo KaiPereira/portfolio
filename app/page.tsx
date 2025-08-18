38f22988963df90fd9109ccd1efe3158f40228e8
Kai Pereira <kaipereira2020@gmail.com>
2025-08-17 22:55:47 -0700

    working on inf scroll

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex justify-between flex-col h-[100px]">
       <div className="w-full h-1/5 bg-black"></div>
-      <div className="h-3/5 text-6xl text-nowrap">
+      <div className="h-3/5 text-6xl text-nowrap infinite-scroll">
         WELCOME! WELCOME! WELCOME! WELCOME! WELCOME! WELCOME! WELCOME! WELCOME! WELCOME!
       </div>
       <div className="w-full h-1/5 bg-black"></div>
