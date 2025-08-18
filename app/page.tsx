73ced30ee627ab38f6d1373805be946179fc7bd2
Kai Pereira <kaipereira2020@gmail.com>
2025-08-17 22:44:45 -0700

    starting the site

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <p className="font-mono text-9xl font-bold">Welcome to my World!</p>
+    <div className="flex justify-between flex-col h-[100px]">
+      <div className="w-full h-1/5 bg-black"></div>
+      <div className="h-3/5 text-6xl text-nowrap">
+        WELCOME! WELCOME! WELCOME! WELCOME! WELCOME! WELCOME! WELCOME! WELCOME! WELCOME!
+      </div>
+      <div className="w-full h-1/5 bg-black"></div>
+    </div>
   );
 }
