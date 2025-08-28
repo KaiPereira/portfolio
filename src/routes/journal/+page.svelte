e34db12c4fe58a50b2adfe5c98a28af692342269
Kai Pereira <kaipereira2020@gmail.com>
2025-08-28 14:15:47 -0700

    I'm loving my new website

--- src/routes/journal/+page.svelte
+++ src/routes/journal/+page.svelte
@@ -1,7 +1,7 @@
 <main class="flex justify-center py-10">
 	<div class="w-full max-w-4xl">
 		<h1>My Journal</h1>
-		<p>I love writing about my experiences and projects I'm working on, so here you can learn more about what I'm making and my life! - Kai</p>
+		<p>I love writing about my experiences meeting new people and projects I'm working on, so here you can learn more about what I'm making and my life! - Kai</p>
 		<div class="flex flex-col gap-4 mt-10">
 			<div>
 				<h2 class="mt-0 text-lg">
