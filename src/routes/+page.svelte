979c66c6beee7e11cc52a6b3005dbae6db0f6bdf
Kai Pereira <kaipereira2020@gmail.com>
2025-08-28 00:07:40 -0700

    starting a journal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,8 +6,5 @@
 	{@html data.readmeHtml}
 </main>
 
-<p>
-	Check out all my projects on
-	<a href="https://github.com/kaipereira" target="_blank" rel="noreferrer">GitHub</a>!
-</p>
-
+<p>Check out all my projects on <a href="https://github.com/kaipereira" target="_blank" rel="noreferrer">GitHub</a>!</p>
+<p>I also journal all my thoughts and work <a href="/journal" target="_blank" rel="noreferrer">here</a>!</p>
