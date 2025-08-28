8dc4bbf6d776745a7605886f8cfe7c84eb723703
Kai Pereira <kaipereira2020@gmail.com>
2025-08-28 13:52:03 -0700

    a little summary for my blog

--- src/routes/journal/+page.svelte
+++ src/routes/journal/+page.svelte
@@ -1,3 +1,6 @@
-<main class="flex items-center">
-	<div class="w-full bg-black">Hello world!</div>
+<main class="flex justify-center py-10">
+	<div class="w-full max-w-3xl">
+		<h1>My Journal</h1>
+		<p>I love to build cool projects and meet new people, so here you can learn more about what I'm making and my life! - Kai
+	</div>
 </main>
