8becd2396086dba778ee8bbdf3a6793b262de0fd
Kai Pereira <kaipereira2020@gmail.com>
2025-08-28 00:18:23 -0700

    let's disable that for now

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,4 +7,4 @@
 </main>
 
 <p>Check out all my projects on <a href="https://github.com/kaipereira" target="_blank" rel="noreferrer">GitHub</a>!</p>
-<p>I also journal all my thoughts and work <a href="/journal" target="_blank" rel="noreferrer">here</a>!</p>
+<!--<p>I also journal all my thoughts and work <a href="/journal" target="_blank" rel="noreferrer">here</a>!</p>-->
