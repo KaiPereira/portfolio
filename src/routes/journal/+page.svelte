41763f93de43cc39b2c7bc65d18d9eb1968ca1b5
Kai Pereira <kaipereira2020@gmail.com>
2025-08-28 14:13:46 -0700

    some stuff

--- src/routes/journal/+page.svelte
+++ src/routes/journal/+page.svelte
@@ -1,6 +1,20 @@
 <main class="flex justify-center py-10">
-	<div class="w-full max-w-3xl">
+	<div class="w-full max-w-4xl">
 		<h1>My Journal</h1>
-		<p>I love to build cool projects and meet new people, so here you can learn more about what I'm making and my life! - Kai
+		<p>I love writing about my experiences and projects I'm working on, so here you can learn more about what I'm making and my life! - Kai</p>
+		<div class="flex flex-col gap-4 mt-10">
+			<div>
+				<h2 class="mt-0 text-lg">
+					<a href="/journal/china" class="text-black">Running a popup cafe in China</a>
+				</h2>
+				<p>In April 2025, I travelled to China alongside 100 other hackers from around the world to run a juice cafe and showcase some games we made! One of the coolest experiences of my life :D</p>
+			</div>
+			<div>
+				<h2 class="mt-0 text-lg">
+					<a href="/journal/boreal" class="text-black">All aboard the Boreal Express</a>
+				</h2>
+				<p>The summer of 2024, I was invited to a cross-country hackathon on a train. This journey transformed my thinking and taught me that I can build whatever I want and to go against the norms.</p>
+			</div>
+		</div>
 	</div>
 </main>
