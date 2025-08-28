0a984289729040968fc26d12a7ba2c0d8a834aad
Kai Pereira <kaipereira2020@gmail.com>
2025-08-27 22:59:59 -0700

    ported the old site to svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import { onMount } from "svelte";
+  import { marked } from "marked";
+
+  let readmeHtml = "";
+
+  onMount(async () => {
+    const url = "https://raw.githubusercontent.com/KaiPereira/KaiPereira/main/README.md";
+
+    try {
+      const res = await fetch(url);
+      if (!res.ok) throw new Error(`Could not fetch README :(, ${res.status}`);
+
+      let result = await res.text();
+      result = result.replaceAll("@", " (at) ");
+      readmeHtml = marked.parse(result);
+    } catch (error) {
+      console.error(error.message);
+      readmeHtml = "<p>Failed to load README.</p>";
+    }
+  });
+</script>
+
+<svelte:head>
+  <title>Kai Pereira :D</title>
+  <meta charset="utf-8" />
+  <meta name="description" content="Hey there, I'm Kai! I'm a 17 year old maker from Canada hacking on PCB's and building hardware!" />
+  <link rel="author" href="Kai Pereira" />
+  <link rel="canonical" href="https://kaipereira.com/" />
+</svelte:head>
+
+<main>
+  {@html readmeHtml}
+</main>
+
+<p>
+  Check out all my projects on
+  <a href="https://github.com/kaipereira" target="_blank" rel="noreferrer">GitHub</a>!
+</p>
+
