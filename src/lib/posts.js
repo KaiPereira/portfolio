dfcab3e91b710061453670589370f8332377f8c1
Kai Pereira <kaipereira2020@gmail.com>
2025-08-28 18:02:39 -0700

    let's see if this works

--- src/lib/posts.js
+++ src/lib/posts.js
@@ -1,35 +1,27 @@
-import fs from "fs";
-import path from "path";
 import matter from "gray-matter";
 import { marked } from "marked";
 
-const postsDir = path.join(process.cwd(), "src/lib/posts");
+const files = import.meta.glob("/src/lib/posts/*.md", { eager: true, as: "raw" });
 
-export function getAllPosts() {
-	return fs.readdirSync(postsDir)
-		.map((filename) => {
-			const filePath = path.join(postsDir, filename);
-			const fileContent = fs.readFileSync(filePath, "utf-8");
-			const { data, content } = matter(fileContent);
-
-			return {
-				slug: filename.replace(".md", ""),
-				metadata: data,
-				content: marked(content)
-			};
-		})
-		.sort((a, b) => new Date(b.metadata.date) - new Date(a.metadata.date));
-}
-
-export function getPostBySlug(slug) {
-	const filePath = path.join(postsDir, slug + ".md");
-	const fileContent = fs.readFileSync(filePath, "utf-8");
-	const { data, content } = matter(fileContent);
+function parseMarkdown(raw, filepath) {
+	const { data, content } = matter(raw);
 
 	return {
-		slug,
+		slug: filepath.split("/").pop().replace(".md", ""),
 		metadata: data,
 		content: marked(content)
 	};
+}
+
+const posts = Object.entries(files).map(([path, raw]) => parseMarkdown(raw, path));
+
+export function getAllPosts() {
+	return posts.sort(
+		(a, b) => new Date(b.metadata.date) - new Date(a.metadata.date)
+	);
+}
+
+export function getPostBySlug(slug) {
+	return posts.find((post) => post.slug === slug);
 }
 
