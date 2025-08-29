48736c5fbd3011619a8b4ccd531e40bf8cb41649
Kai Pereira <kaipereira2020@gmail.com>
2025-08-28 17:41:31 -0700

    made a global layout which was fun

---
+++ src/routes/journal/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<main class="flex justify-center py-10">
+	<div class="w-full max-w-4xl">
+		{@render children()}
+	</div>
+</main>
+
--- src/routes/journal/+page.svelte
+++ src/routes/journal/+page.svelte
@@ -2,20 +2,16 @@
 	export let data;
 </script>
 
-<main class="flex justify-center py-10">
-	<div class="w-full max-w-4xl">
-		<h1>My Journal</h1>
-		<p>I love writing about my experiences meeting new people and projects I'm working on, so here you can learn more about what I'm making and my life! - Kai</p>
-		<div class="flex flex-col gap-4 mt-10">
-		{#each data.posts as post}
-			<div>
-				<div class="flex gap-4">
-					<h2 class="mt-0 text-lg"><a href={`/journal/${post.slug}`} class="text-black">{post.metadata.title}</a></h2>
-					<p class="m-1">{post.metadata.date}</p>
-				</div>
-				<p class="mt-0">{post.metadata.description}</p>
-			</div>
-		{/each}
+<h1>My Journal</h1>
+<p>I love writing about my experiences meeting new people and projects I'm working on, so here you can learn more about what I'm making and my life! - Kai</p>
+<div class="flex flex-col gap-4 mt-10">
+{#each data.posts as post}
+	<div>
+		<div class="flex gap-4">
+			<h2 class="mt-0 text-lg"><a href={`/journal/${post.slug}`} class="text-black">{post.metadata.title}</a></h2>
+			<p class="m-1">{post.metadata.date}</p>
 		</div>
+		<p class="mt-0">{post.metadata.description}</p>
 	</div>
-</main>
+{/each}
+</div>
