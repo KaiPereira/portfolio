afbb08afcc3ba91394842601b120815ebbba5833
Kai Pereira <kaipereira2020@gmail.com>
2025-08-28 17:18:34 -0700

    dynamic loading

---
+++ src/lib/posts.js
@@ -0,0 +1,33 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { marked } from "marked";
+
+const postsDir = path.join(process.cwd(), "src/lib/posts");
+
+export function getAllPosts() {
+	return fs.readdirSync(postsDir).map((filename) => {
+		const filePath = path.join(postsDir, filename);
+		const fileContent = fs.readFileSync(filePath, "utf-8");
+		const { data, content } = matter(fileContent);
+
+		return {
+			slug: filename.replace(".md", ""),
+			metadata: data,
+			content: marked(content)
+		};
+	});
+}
+
+export function getPostBySlug(slug) {
+	const filePath = path.join(postsDir, slug + ".md");
+	const fileContent = fs.readFileSync(filePath, "utf-8");
+	const { data, content } = matter(fileContent);
+
+	return {
+		slug,
+		metadata: data,
+		content: marked(content)
+	};
+}
+
--- src/routes/journal/+page.svelte
+++ src/routes/journal/+page.svelte
@@ -1,20 +1,20 @@
+<script>
+	export let data;
+</script>
+
 <main class="flex justify-center py-10">
 	<div class="w-full max-w-4xl">
 		<h1>My Journal</h1>
 		<p>I love writing about my experiences meeting new people and projects I'm working on, so here you can learn more about what I'm making and my life! - Kai</p>
 		<div class="flex flex-col gap-4 mt-10">
+		{#each data.posts as post}
 			<div>
 				<h2 class="mt-0 text-lg">
-					<a href="/journal/china" class="text-black">Running a popup cafe in China</a>
+					<a href={`/journal/${post.slug}`} class="text-black">{post.metadata.title}</a>
 				</h2>
-				<p>In April 2025, I travelled to China alongside 100 other hackers from around the world to run a juice cafe and showcase some games we made! One of the coolest experiences of my life :D</p>
+				<p>{post.metadata.description}</p>
 			</div>
-			<div>
-				<h2 class="mt-0 text-lg">
-					<a href="/journal/boreal" class="text-black">All aboard the Boreal Express</a>
-				</h2>
-				<p>The summer of 2024, I was invited to a cross-country hackathon on a train. This journey transformed my thinking and taught me that I can build whatever I want and to go against the norms.</p>
-			</div>
+		{/each}
 		</div>
 	</div>
 </main>
