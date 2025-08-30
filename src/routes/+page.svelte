dd8afede24839e4ee2dca6bddb7071718835cf38
Kai Pereira <kaipereira2020@gmail.com>
2025-08-30 12:44:20 -0700

    updating the site to make it flow better

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,5 +6,4 @@
 	{@html data.readmeHtml}
 </main>
 
-<p>Check out all my projects on <a href="https://github.com/kaipereira" target="_blank" rel="noreferrer">GitHub</a>!</p>
-<p>Or take a gander at my writing <a href="/journal">here</a>!</p>
+<p>Check out all my projects on <a href="https://github.com/kaipereira" target="_blank" rel="noreferrer">GitHub</a> or take a gander at my <a href="/journal">journal</a>!</p>
