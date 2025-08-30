e04fa5b7b641d9bbdcbd17e4344ccaa5351d5a57
Kai Pereira <kaipereira2020@gmail.com>
2025-08-30 14:53:58 -0700

    adding meta tags for each page

--- src/routes/journal/[slug]/+page.svelte
+++ src/routes/journal/[slug]/+page.svelte
@@ -1,6 +1,14 @@
 <script>
 	export let data;
+	const { post } = data;
 </script>
+
+<svelte:head>
+	<title>{post.metadata.title}</title>
+	<meta name="description" content={post.metadata.description} />
+	<meta property="og:title" content={post.metadata.title} />
+	<meta property="og:description" content={post.metadata.description} />
+</svelte:head>
 
 <a href="/journal">[Back to my Journal]</a>
 <div class="markdown">{@html data.post.content}</div>
