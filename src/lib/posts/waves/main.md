59ff021f33aadd70f03316a17ec04b63c69b2ac0
Kai Pereira <kaipereira2020@gmail.com>
2025-12-17 14:04:55 -0800

    some more series formatting

--- src/lib/posts/waves/main.md
+++ src/lib/posts/waves/main.md
@@ -1,7 +1,7 @@
 ---
 title: "The World in Waves"
 date: "2025-12-17"
-description: "A technical explainer of the electromagnetic spectrum, and how waves actually work"
+description: "A technical explainer of the electromagnetic spectrum, and how we create and use these different waves"
 ---
 
 Hello world!
--- src/routes/journal/[slug]/+page.svelte
+++ src/routes/journal/[slug]/+page.svelte
@@ -15,6 +15,15 @@
 {#if post.isMain}
 	<h1>{post.metadata.title}</h1>
 	<p>{post.metadata.description}</p>
+	<div class="chapters">
+		<p>Radio</p><p>•</p>
+		<p>Microwaves</p><p>•</p>
+		<p>Infrared</p><p>•</p>
+		<p>Visible</p><p>•</p>
+		<p>Ultraviolet</p><p>•</p>
+		<p>X-Rays</p><p>•</p>
+		<p>Gamme Rays</p>
+	</div>
 {/if}
 
 <div class="markdown">{@html post.content}</div>
