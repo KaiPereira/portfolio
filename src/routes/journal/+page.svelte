ea950e1a106811035bb87c5612f378ce89b60a1b
Kai Pereira <kaipereira2020@gmail.com>
2026-04-30 06:59:42 -0700

    new site styling!

--- src/routes/journal/+page.svelte
+++ src/routes/journal/+page.svelte
@@ -7,15 +7,15 @@
 	<meta name="description" content="This is a recollection of cool experiences I've had in my life, random stuff I find interesting, and projects I've made!" />
 </svelte:head>
 
-<h1>My Journal</h1>
 <a href="/">[Back to my Homepage]</a>
+<h1>My Journal</h1>
 <p>I love writing about my experiences meeting new people and projects I'm working on, so here you can learn more about what I'm making and my life! - Kai</p>
 <div class="flex flex-col gap-4 mt-10">
 {#each data.posts as post}
 	<div>
 		<div class="flex gap-4 items-center mb-2">
-			<h2 class="m-0 text-lg"><a href={`/journal/${post.slug}`} class="!text-main no-hover">{post.metadata.title}</a></h2>
-			<p class="m-1">{post.metadata.date}</p>
+			<h2 class="m-0 text-2xl"><a href={`/journal/${post.slug}`} class="!text-main no-hover">{post.metadata.title}</a></h2>
+			<p class="!m-4 text-sm">{post.metadata.date}</p>
 		</div>
 		<p class="mt-0">{post.metadata.description}</p>
 	</div>
