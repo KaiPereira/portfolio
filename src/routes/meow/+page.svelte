8a80353dc4b81f9f51bef3dcfd8d77cc14c062e9
Kai Pereira <kaipereira2020@gmail.com>
2026-06-18 15:39:57 -0700

    adding kitites :D

---
+++ src/routes/meow/+page.svelte
@@ -0,0 +1,6 @@
+<img src="/images/cats/furry_1.jpg" class="w-108"/>
+<img src="/images/cats/fluffy_2.jpg" class="w-108" />
+<img src="/images/cats/meowy_3.jpg" class="w-108" />
+<img src="/images/cats/cutiepatootie_4.jpg" class="w-108" />
+
+<p>MEOOWWW!! Follow me on GitHub :D</p>
